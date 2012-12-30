6426a921e6f4b4e40f3e8aa9a00855e7957dd5ac
TangRufus <tangRufus@gmail.com>
2012-12-31 02:08:23 +0800

    added weibo and email icons

--- config.rb
+++ config.rb
@@ -3,16 +3,16 @@ project_type = :stand_alone
 
 # Publishing paths
 http_path = "/"
-http_images_path = "/images"
-http_generated_images_path = "/images"
+http_images_path = "/img"
+http_generated_images_path = "/img"
 http_fonts_path = "/fonts"
 css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
-images_dir = "source/images"
+images_dir = "source/img"
 fonts_dir = "source/fonts"
-generated_images_dir = "source/images"
+generated_images_dir = "source/img"
 
 line_comments = false
 output_style = :compressed
